7f391e70b226ae5552b7876644693f449fd163fa
Alex Eden <alexandereden91@gmail.com>
2024-11-08 22:09:32 -0600

    Got them up and running reallllll quick

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import { Config } from 'tailwindcss';
+
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  plugins: [],
+  theme: {
+    extend: {},
+    // borderRadius: tokens.borderRadius,
+    // boxShadow: tokens.boxShadow,
+    // colors: {
+    //   ...tokens.colors,
+    //   // Semantic colors
+    //   primary: tokens.colors.blue,
+    //   success: tokens.colors.green,
+    //   warning: tokens.colors.red,
+    //   notification: tokens.colors.yellow,
+    // },
+    // fontFamily: tokens.fontFamily,p
+    // fontWeight: tokens.fontWeight,
+    // fontSize: tokens.fontSize,
+    // lineHeight: tokens.lineHeight,
+    // screens: { ...tokens.breakpoints },
+  },
+} satisfies Config;
